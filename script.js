956b0992d2e3313ecb2fb52c04c05b68b87bb7b9
ridwanAdisurya27 <boymargono70@gmail.com>
2025-03-06 08:37:34 +0700

    menambah fungsi random pada main element

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+// Data Station
+const Array_paten= [1,2,3,4,5,6,7,8,9];
+let array = [];
 
+// Undisplayed program
+
+function Random(){
+    while (array.length > 0) {
+        array.pop();
+    }
+    console.log("array kosong " + array);
+    while(array.length <= 8) {
+        let rand = Array_paten[Math.floor(Math.random()*Array_paten.length)];
+        if(array.includes(rand) == false){
+            array.push(rand);
+        }
+    }
+    $("div .grid").each(function(index){
+        $(this).text(array[index]);
+    })
+}
 
 // aside right function
 let spin = 0;
@@ -13,3 +33,5 @@ $("#social-btn").on("click", function () {
         spin = 0;
     }
 })
+
+$("#highscore").on("click", Random);
